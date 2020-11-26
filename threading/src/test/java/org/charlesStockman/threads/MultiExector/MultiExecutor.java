0d0c373cb0659a88425c18e065df397c56de18b6
Charles Stockman <chuck.stockman@yahoo.com>
2020-11-26 03:39:54 -0500

    Fixed the build  and implemented the code for testGenericNameWithDifferentiator

--- threading/src/test/java/org/charlesStockman/threads/MultiExector/MultiExecutor.java
+++ threading/src/test/java/org/charlesStockman/threads/MultiExector/MultiExecutor.java
@@ -3,12 +3,26 @@ package org.charlesStockman.threads.MultiExector;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * A prgoram to verify that MultiExecutor can spawn thread a number of thread
  * and then destroy them.
  */
 
 public class MultiExecutor {
+
+    /**
+     * Adds a differentiator to make names unique.
+     */
+    private AtomicInteger differentiator = new AtomicInteger(0);
+
+    /**
+     * The first of part of the uniqueName;
+     */
+    private String className = this.getClass().getSimpleName();
+
 
     /**
      * Test to verify that a generic name can be created with a number
@@ -20,8 +34,8 @@ public class MultiExecutor {
     @Test
     public void testGenericNameWithDifferentiator() {
 
-        String resultName1 = "MultiExecutor_1";
-        String resultName2 = "MultiExecutor_2";
+        String resultName1 = "MultiExecutor_0";
+        String resultName2 = "MultiExecutor_1";
         String answer, answer2;
 
         answer = generateUniqueNameWithDifferentiator();
@@ -32,5 +46,14 @@ public class MultiExecutor {
 
     }
 
-
+    /**
+     * Creates a generic name ( the class name ) with the differentiator
+     * ( a number ) that make name unique
+     *
+     * @return A string that is a unique name
+     */
+    public String generateUniqueNameWithDifferentiator() {
+        String name = className + "_" + differentiator.getAndIncrement();
+        return name;
+    }
 }
