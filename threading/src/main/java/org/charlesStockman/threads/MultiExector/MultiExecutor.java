8f0dd28c9785dbeee53ea66d681c23e73abce850
Charles Stockman <chuck.stockman@yahoo.com>
2020-11-25 21:47:08 -0500

    Added testGenericNameWithDifferentiator()

---
+++ threading/src/main/java/org/charlesStockman/threads/MultiExector/MultiExecutor.java
@@ -0,0 +1,4 @@
+/**
+ * The client of this tasks will create a list of runnable of Runnable.  Each
+ * runnable class will print out a string and wait to be interuppted.
+ */
---
+++ threading/src/test/java/org/charlesStockman/threads/MultiExector/MultiExecutor.java
@@ -0,0 +1,36 @@
+package org.charlesStockman.threads.MultiExector;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * A prgoram to verify that MultiExecutor can spawn thread a number of thread
+ * and then destroy them.
+ */
+
+public class MultiExecutor {
+
+    /**
+     * Test to verify that a generic name can be created with a number
+     * at the end to make the new name distinct.
+     *
+     * Does two test : Can the name be generated and after the name is
+     * generated can a different name be generated.
+     */
+    @Test
+    public void testGenericNameWithDifferentiator() {
+
+        String resultName1 = "MultiExecutor_1";
+        String resultName2 = "MultiExecutor_2";
+        String answer, answer2;
+
+        answer = generateUniqueNameWithDifferentiator();
+        Assertions.assertEquals(resultName1, answer);
+
+        answer2 = generateUniqueNameWithDifferentiator();
+        Assertions.assertEquals(resultName2, answer);
+
+    }
+
+
+}
